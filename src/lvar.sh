b997a23ee201da20ec55102e4a3ead5768b41d03
tohu.masao <konoe_mario@yahoo.co.jp>
2019-08-08 19:01:54 +0900

    任意のアセンブラを実行できるようにする

--- src/lvar.sh
+++ src/lvar.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 # 変数割り当ての確認をするためのアセンブリ確認用
+target="$1"
+
+if [ -n "$target" ]; then
+echo "exec $target"
+gcc -o "$target" "$target".s 
+./$target
+else
 gcc -o lvar lvar.s
 ./lvar
+fi
+
 echo $?
